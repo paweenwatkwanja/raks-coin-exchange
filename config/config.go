7ca767918c459d033f3d492cbf1f5b118e06b70b
paquesqueue <paweenwat.kwanja@gmail.com>
2025-11-06 15:13:36 +0700

    feat: second draft

--- config/config.go
+++ config/config.go
@@ -6,14 +6,16 @@ import (
 )
 
 type Config struct {
-	AppHost string
-	AppPort string
+	AppHost  string
+	AppPort  string
+	Endpoint string
 }
 
 func LoadConfig() *Config {
 	fmt.Println("Loading configs")
 	return &Config{
-		AppHost: os.Getenv("APP_HOST"),
-		AppPort: os.Getenv("APP_PORT"),
+		AppHost:  os.Getenv("APP_HOST"),
+		AppPort:  os.Getenv("APP_PORT"),
+		Endpoint: os.Getenv("ENDPOINT"),
 	}
 }
--- internal/transaction/transaction_handler.go
+++ internal/transaction/transaction_handler.go
@@ -2,11 +2,16 @@ package handler
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/paweenwatkwanja/raks-coin-exchange/config"
+	broadcast "github.com/paweenwatkwanja/transaction-broadcasting"
+	"github.com/paweenwatkwanja/transaction-broadcasting/models"
 )
 
 type transactionHandler struct {
+	config config.Config
 }
 
 func NewTransactionHandler(r fiber.Router) {
@@ -15,6 +20,37 @@ func NewTransactionHandler(r fiber.Router) {
 }
 
 func (h *transactionHandler) postTransaction(c *fiber.Ctx) error {
-	fmt.Println("Hello, World!")
+	request := models.BroadcastRequest{}
+	err := c.BodyParser(&request)
+	if err != nil {
+		fmt.Printf("error: %v\n", err.Error())
+	}
+
+	broadcastService := broadcast.NewBroadcastService()
+
+	broadcastService.WithRetryAttempt(5)
+	broadcastService.WithRetryDuration(time.Duration(10))
+
+	endpoint := h.config.Endpoint
+	broadcastURL := fmt.Sprintf("%v/%v", endpoint, "broadcast")
+
+	txHash, err := broadcastService.BroadcastTransaction(broadcastURL, request)
+	if err != nil {
+		fmt.Printf("error: %v\n", err.Error())
+	}
+	fmt.Printf("txHash: %v\n", txHash)
+
+	monitorURL := fmt.Sprintf("%v/%v/%v", endpoint, "check", txHash)
+	txStatus, err := broadcastService.MonitorTransaction(monitorURL)
+	if err != nil {
+		fmt.Printf("error: %v\n", err.Error())
+	}
+	fmt.Printf("txStatus: %v\n", txStatus)
+
+	err = broadcastService.HandleStatus(monitorURL, txStatus)
+	if err != nil {
+		fmt.Printf("error: %v\n", err.Error())
+	}
+	fmt.Println("done")
 	return nil
 }
